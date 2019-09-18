861105ea55b22b83ff2f72358c08f9a4b4fa7431
JSalazr <jonathansalazar96@gmail.com>
2019-09-18 11:41:21 -0600

    Two numbers passing, multiple numbers failing.

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -6,23 +6,20 @@ namespace Kata.Spec
     public class when_feeding_the_monkey
     {
         static Monkey _systemUnderTest;
-        
-        Establish context = () => 
+
+        Establish context = () =>
             _systemUnderTest = new Monkey();
 
-        Because of = () => 
+        Because of = () =>
             _systemUnderTest.Eat("banana");
 
         It should_have_the_food_in_its_belly = () =>
             _systemUnderTest.Belly.Should().Contain("banana");
     }
-    
+
     public class when_user_input_is_zero
     {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
         Because of = () => { _result = _systemUnderTest.Add(); };
         It should_return_zero = () => { _result.Should().Be(0); };
@@ -32,10 +29,7 @@ namespace Kata.Spec
 
     public class when_user_input_is_one_number
     {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
         Because of = () => { _result = _systemUnderTest.Add("3"); };
         It should_return_the_same_number = () => { _result.Should().Be(3); };
@@ -45,20 +39,28 @@ namespace Kata.Spec
 
     public class when_user_input_is_two_numbers
     {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
         Because of = () => { _result = _systemUnderTest.Add("1,3"); };
         It should_return_sum_of_all = () => { _result.Should().Be(4); };
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class when_user_input_is_unknown_amount
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
+        It should_return_sum_of_all_numbers = () => { _result.Should().Be(6); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
 }
 
-//    2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
-//    3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 //    4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
 //    5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 //    6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -6,7 +6,10 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            return 3;
+            var numbers = s.Split(",");
+            if (numbers.Length == 1)
+                return int.Parse(numbers[0]);
+            return int.Parse(numbers[0]) + int.Parse(numbers[1]);
         }
     }
 }
