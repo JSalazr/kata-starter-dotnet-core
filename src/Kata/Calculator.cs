086e4fe139a2ee00d31f49ba446eecd34114a9b2
afloresmorales <afloresmorales96@gmail.com>
2019-09-23 11:42:05 -0600

    Refactoring to methods.

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Kata
@@ -9,22 +10,32 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(s))
                 return 0;
-            var separator = new []{",", "\n"};
-            if (s.StartsWith("//"))
-            {    
-                // //[*][%]\n1*2%3
-                // *]%]
-                var strings = s.Split("\n");
-                separator = strings.First().Replace("//", "").Replace("[","").Split("]");
-                s = strings.Last();
-            }
-            var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse).Where(x=>x<1001);
+            var numbers = GetDelimiters(s);
+            CheckNegatives(numbers);
+            return numbers.Sum();
+        }
+
+        static void CheckNegatives(IEnumerable<int> numbers)
+        {
             var negatives = numbers.Where(x => x < 0);
             if (negatives.Any())
             {
                 throw new Exception($@"Negatives not allowed: {string.Join(", ", negatives)}");
             }
-            return numbers.Sum();
+        }
+
+        static IEnumerable<int> GetDelimiters(string s)
+        {
+            var separator = new[] {",", "\n"};
+            if (s.StartsWith("//"))
+            {
+                var strings = s.Split("\n");
+                separator = strings.First().Replace("//", "").Replace("[", "").Split("]");
+                s = strings.Last();
+            }
+
+            var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse).Where(x => x < 1001);
+            return numbers;
         }
     }
 }
