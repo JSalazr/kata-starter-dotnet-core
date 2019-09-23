e0347f16c366ceec0f53f09127d2262a36c79e38
afloresmorales <afloresmorales96@gmail.com>
2019-09-23 11:35:10 -0600

    multichar delimiter passing, multing custom delimiters failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -13,17 +13,16 @@ namespace Kata
             if (s.StartsWith("//"))
             {
                 var strings = s.Split("\n");
-                separator = new[] {strings.First().Replace("//", "")};
+                separator = new[] {strings.First().Replace("//", "").Replace("[","").Replace("]","")};
                 s = strings.Last();
             }
-            var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse);
+            var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse).Where(x=>x<1001);
             var negatives = numbers.Where(x => x < 0);
-            var valid_numbers = numbers.Where(x => x > 0 && x < 1001);
             if (negatives.Any())
             {
                 throw new Exception($@"Negatives not allowed: {string.Join(", ", negatives)}");
             }
-            return valid_numbers.Sum();
+            return numbers.Sum();
         }
     }
 }
